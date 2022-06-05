2c4f9af4d975f72fa5f5725517c5a03ab930fdea
wendyvosu <102492464+wendyvosu@users.noreply.github.com>
2022-06-05 19:41:05 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+// greeting express app
 app.get("/greeting", (req, res) => {
     res.send("Welcome, stranger");
 })
 
 app.get("/greeting/:name", (req, res) => {
-    console.log(req.params.name)
     res.send(`What's up, ${req.params.name}`)
+})
+
+// tip calculator express app
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    res.send(`${req.params.total * req.params.tipPercentage * .01}`)
 })
 
 app.listen(port, () => {
